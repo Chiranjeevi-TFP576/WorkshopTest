88e16da099aa3d4d6a8c7755b9c37e6acad4b0f2
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-10-31 20:12:42 +0530

    BinarytoDecimal Afterchanges

---
+++ BinaryToDecimal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop
+{
+    internal class BinaryToDecimal
+    {
+        public static void Convertion()
+        {
+            int num, binVal, decVal = 0, baseVal = 1, rem;
+            num = 101; //5
+            binVal = num;
+            while (num > 0) 
+            {
+                rem = num % 10;
+                decVal = decVal+rem * baseVal;
+                num = num / 10;//num /=10
+                baseVal = baseVal * 2;
+            }
+            Console.WriteLine(" Binary Number" + binVal);
+            Console.WriteLine("\n Decimal Number" + decVal );
+            
+
+            
+        }
+    }
+}
